4b782c8332cfd6d6b3441413baaa19c908e6de6c
Rodrigo Camio <rodricamio94@gmail.com>
2023-11-14 21:08:02 -0300

    modificaciones en el vagrant file y en el bootstrap, para que instale Docker

--- boostrap.sh
+++ boostrap.sh
@@ -3,3 +3,12 @@ apt install python-pip
 apt install ansible -y
 apt install awscli
 export PATH="/home/vagrant/.local/bin:$PATH"
+
+# DOCKER INSTALL
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh --dry-run
+sudo sh get-docker.sh
+# USE DOCKER WITHOUT SUDO
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+newgrp docker
